b910aa712ce2727f96c48ef571b30897a1e531d1
sonal <sonalkarle01@gmail.com>
2021-03-11 00:17:02 +0530

    Retrive data from sql database

--- Ado.Net/EmployeeDetails.cs
+++ Ado.Net/EmployeeDetails.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Ado.Net
+namespace ConnectionTest
 {
-    class Employeetable
+     public class Employee
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Gender { get; set; }
-        public double PhoneNumber { get; set; }
-        public DateTime StartDate { get; set; }
-        public string Department { get; set; }
-        public double BasicPay { get; set; }
-        public double Deduction { get; set; }
-        public double TaxablePay { get; set; }
-        public double IncomeTax { get; set; }
-        public double NetPay { get; set; }
+        public int id;
+        public string name;
+        public int basicPay;
+        public DateTime startDate;
+        public char gender;
+        public long phoneNumber;
+        public string address;
+        public string department;
+        public int deduction;
+        public int taxablePay;
+        public int incomeTax;
+        public int netPay;
+        
+
+       
     }
 }
--- Ado.Net/Program.cs
+++ Ado.Net/Program.cs
@@ -1,35 +1,61 @@
-﻿using System;
+﻿using ConnectionTest;
+using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace Ado.Net
+namespace EmployeePayrollService
 {
-    class Program
+    public class EmployeePayroll
     {
-        static string connectionString = @"Data Source=DESKTOP-OKP25QH;Initial Catalog=employee_Payroll";
+        static string connectionString = "data Source=DESKTOP-OKP25QH;Initial Catalog=employee_Payroll;Integrated security=SSPI";
 
-        public static DataSet GetAllEmployeeData()
+
+
+        public List<Employee> ReturnAllRecords()
         {
-            Employeetable employee = new Employeetable();
+            List<Employee> employees = new List<Employee>();
+            Employee emp = new Employee();
             SqlConnection connection = new SqlConnection(connectionString);
-            try
+            string query = @"Select ID,Name,BasicPay,StartDate,Gender,PhoneNumber,Address,Department,Deduction,TaxablePay,IncomeTax,NetPay from  employee_Payroll";
+
+            using (connection)
             {
-                using (connection)
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+               
+                connection.Open();
+                SqlDataReader dr = sqlCommand.ExecuteReader();
+                if (dr.HasRows)
                 {
-                    connection.Close();
+                    while (dr.Read())
+                    {
+                        emp.id = dr.GetInt32(0);
+                        emp.name = dr.GetString(1);
+                        emp.basicPay = dr.IsDBNull(7) ? 0 : dr.GetInt32(7);
+                        emp.startDate = dr.GetDateTime(11);
+                        emp.gender = Convert.ToChar(dr.GetString(3));
+                        emp.phoneNumber = dr.IsDBNull(4) ? 0 : dr.GetInt64(4);
+                        emp.address = dr.IsDBNull(5) ? "" : dr.GetString(5);
+                        emp.department = dr.GetString(6);
+                        emp.deduction = dr.IsDBNull(8) ? 0 : dr.GetInt32(8);
+                        emp.taxablePay = dr.IsDBNull(9) ? 0 : dr.GetInt32(9);
+                        emp.incomeTax = dr.IsDBNull(10) ? 0 : dr.GetInt32(10);
+                        emp.netPay = dr.IsDBNull(11) ? 0: dr.GetInt32 (11);
+                        employees.Add(emp);
+                        Console.WriteLine(emp.id + "," + emp.name + "," + emp.phoneNumber);
+                    }
+                    
                 }
+                connection.Close();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            return null;
+            return employees;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to Employee Payroll Service");
-            GetAllEmployeeData();
+            EmployeePayroll emproll = new EmployeePayroll();
+            emproll.ReturnAllRecords();
         }
     }
 }
+
